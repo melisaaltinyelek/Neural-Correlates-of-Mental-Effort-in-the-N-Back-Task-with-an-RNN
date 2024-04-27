2e35a0e18c268d55a4bde6ea00d383ef86e1fd54
Melisa Altinyelek <melisaaltinyelek@hotmail.com>
2024-04-27 21:22:17 +0200

    updated letter list, duplicated a random letter

--- n-back_data.py
+++ n-back_data.py
@@ -5,25 +5,41 @@ import random
 
 class NbackCreator():
     def __init__(self):
-        global consonants
-        consonants = "bcdfghjklmnprstvwxyz"
-        self.consonants = consonants
-        
-        
-    def create_sequences(self):
-        self.cons_sequence = []
 
-        for self.consonant in range(len(consonants)):
-            self.consonant = random.choice(consonants)
-            self.cons_sequence.append(self.consonant)
-        
-        return self.cons_sequence
+        """
+        Stores all consonants in a list.
+        """
+
+        self.consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "r", "s", "t", "v", "w", "x", "y", "z"]
+
+    def generate_sequence(self):
+
+        """
+        Given a consonant list, picks a random letter and duplicates it.
+        Appends that duplicate to the list.
+        """
+
+        random.shuffle(self.consonants)
+        self.chosen_letter = random.choice(self.consonants)
+
+        self.consonants.append(self.chosen_letter)
+
+
+        return self.consonants
+
+
+    def is_match(self, sequence):
+        None
+
+    def generate_trials(self):
+        None
+
     
 #%%
 
 n_back = NbackCreator()
 
-sequence = n_back.create_sequences()
+sequence = n_back.generate_sequence()
 
 print(sequence)
 # %%
