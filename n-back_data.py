19405bb3aff936327f4b9b6e651d19b533d9aee9
Melisa Altinyelek <melisaaltinyelek@hotmail.com>
2024-05-01 08:42:57 +0200

    3-back rule and mismatch condition

--- n-back_data.py
+++ n-back_data.py
@@ -1,5 +1,8 @@
 #%%
+
 import random
+from random import randrange
+import numpy as np
 
 #%%
 
@@ -10,49 +13,63 @@ class NbackCreator():
         Stores all consonants in a list.
         """
 
-        self.consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "r", "s", "t", "v", "w", "x", "y", "z"]
+        self.consonants = ["B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "R", "S", "T", "V", "W", "X", "Y", "Z"]
 
-    def generate_sequence(self):
+    def is_match(self, num_sequences):
 
         """
-        Given a consonant list, picks a random letter and duplicates it.
-        Appends that duplicate to the list.
+        Given a consonant list, picks a random letter from the random consonant_copy list and duplicates it.
+        Appends that duplicate to the list and places it based on the 3-back rule.
         """
 
-        random.shuffle(self.consonants)
-        self.chosen_letter = random.choice(self.consonants)
+        match_sequences = []
 
-        #self.consonants.append(self.chosen_letter)
+        for _ in range(num_sequences):
+            match_consonants_copy = self.consonants.copy()
+            random.shuffle(match_consonants_copy)
+            chosen_letter = random.choice(match_consonants_copy)
+            idx_chosen_letter = match_consonants_copy.index(chosen_letter)
+            match_consonants_copy.insert(idx_chosen_letter + 3, chosen_letter)
+            match_sequences.append((idx_chosen_letter, match_consonants_copy)) 
 
-        # Access the index of the chosen letter in the original list
-        idx_chosen_letter = self.consonants.index(self.chosen_letter)
+        return match_sequences
+    
+
+    def is_mismatch(self, num_sequences):
+
+        """
+        Given a consonant list, picks a random letter from the random consonant_copy list and duplicates it.
+        Appends that duplicate to the list and places it without satisfying the 3-back rule.
+        The duplicated letter is randomly placed in the list.
+        """
+
+        mismatch_sequences = []
         
-        # Access the last index of the list which represent the duplicate letter
-        #idx_duplicate_letter = len(self.consonants) - 1
-
-        # Insert the chosen letter (its duplicaste) 3 indices after its first occurence
-        self.consonants.insert(idx_chosen_letter + 3, self.chosen_letter)
-
-        return idx_chosen_letter, self.consonants
-
+        for _ in range(num_sequences):
+            mismatch_consonants_copy = self.consonants.copy()
+            random.shuffle(mismatch_consonants_copy)
+            chosen_letter = random.choice(mismatch_consonants_copy)
+            idx_chosen_letter = mismatch_consonants_copy.index(chosen_letter)
+            mismatch_consonants_copy.insert(randrange(len(mismatch_consonants_copy)), chosen_letter)
+            mismatch_sequences.append((idx_chosen_letter, mismatch_consonants_copy))
         
-
-    def is_match(self, sequence):
-        None
-
-    def generate_trials(self):
-        None
+        return mismatch_sequences
 
     
 #%%
 
 if __name__ == "__main__":
-    
+
     n_back = NbackCreator()
+    num_sequences = 22
+     
+    match_sequences = n_back.is_match(num_sequences)
+    mismatch_sequences = n_back.is_mismatch(num_sequences)
 
-    for _ in range(len(n_back.consonants)):
-        
-        sequence = n_back.generate_sequence()
-
+    for idx, sequence in enumerate(match_sequences):
         print(sequence)
+        
+    for idx_2, sequence_2 in enumerate(mismatch_sequences):
+        print(sequence_2)
+
 # %%
