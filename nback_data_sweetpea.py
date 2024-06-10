b799e0a18fc2f5e2c810f8d154e18bdb6056d923
Melisa Altinyelek <melisaaltinyelek@hotmail.com>
2024-06-10 18:31:06 +0200

    concatenated all csv files into pd dataframe

---
+++ nback_data_sweetpea.py
@@ -0,0 +1,77 @@
+#%%
+
+import pandas as pd
+import glob
+
+from sweetpea import (
+    Factor, DerivedLevel, WithinTrial, Transition, AtMostKInARow,
+    MultiCrossBlock, CrossBlock, synthesize_trials, print_experiments,
+    CMSGen, IterateGen, RandomGen, MinimumTrials, tabulate_experiments
+)
+
+from sweetpea import *
+
+#%%
+
+# PARAMETERS
+
+N = 3 # N = 3 for 3-back task
+num_sequences = 5 # Number of sequences
+num_minimum_trials = 50 # Minimum number of trials per sequence
+
+### REGULAR FACTORS
+
+letters = ["A", "B", "C", "D", "E", "F"]
+
+# Factor is used to define factors and its levels
+curr_letter = Factor("letter", letters)
+
+### DERIVED FACTORS
+
+# Response factor
+
+def is_target(letter):
+    return letter[0] == letter[-(N-1)] # If the letter N trials back matches the letter on the current trial, it's a target
+def is_no_target(letter):
+    return not is_target(letter)
+
+response = Factor("response", [
+    DerivedLevel("target", Window(is_target, [curr_letter], 3, 1)),
+    DerivedLevel("no target",  Window(is_no_target, [curr_letter], 3, 1)),
+])
+
+### EXPERIMENT
+
+k = 7
+constraints = [
+               MinimumTrials(num_minimum_trials)]
+
+design       = [curr_letter, response]
+crossing     = [curr_letter, response]
+block        = CrossBlock(design, crossing, constraints)
+
+experiments  = synthesize_trials(block, num_sequences, CMSGen)
+
+### END OF EXPERIMENT DESIGN
+
+print_experiments(block, experiments)
+
+#tabulate_experiments(block, experiments, [curr_letter, response])
+
+save_experiments_csv(block, experiments, "n_back_sequence")
+# %%
+
+# Store all csv files in a list for the current directory
+csv_files = glob.glob("*.csv")
+
+# Create an empty dataframe to store the combined data
+combined_df = pd.DataFrame()
+
+# Loop through each csv file and append its contents to the dataframe
+for csv_file in csv_files:
+    df = pd.read_csv(csv_file)
+    combined_df = pd.concat([combined_df.fillna("response"), df])
+    
+combined_df
+
+# %%
