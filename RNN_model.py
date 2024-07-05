8f6e5537d9bfde32464bec5ac481eeba3b94cb58
Melisa Altinyelek <melisaaltinyelek@hotmail.com>
2024-07-05 16:54:37 +0200

    splitted the data into training, val and test sets

--- RNN_model.py
+++ RNN_model.py
@@ -41,26 +41,56 @@ def preprocess_data(data_as_input):
     return df
 
 dataset = preprocess_data("data.csv")
-dataset.to_csv("new_data.csv", index = False)
+#dataset.to_csv("new_data.csv", index = False)
+
+#%%
+
+def split_data(data_to_split):
+
+    df = pd.read_csv(data_to_split)
+
+    letters = np.array(df["letter"].tolist())
+    responses = np.array(df["response"].tolist())
+
+    train_size = int(0.8 * len(df))
+    val_size = int(0.1 * len(df))
+    test_size = len(df) - train_size - val_size
+
+    X_train = letters[:train_size]
+    y_train = responses[:train_size]
+
+    X_val = letters[train_size:val_size + train_size]
+    y_val = responses[train_size:val_size + train_size]
+
+    X_test = letters[train_size + val_size:]
+    y_test = responses[train_size + val_size:]
+
+    return X_train, y_train, X_val, y_val, X_test, y_test
+
+X_train, y_train, X_val, y_val, X_test, y_test = split_data("n_back_data.csv")
+
+print("Training dataset shape:", X_train.shape, y_train.shape)
+print("Validation dataset shape:", X_val.shape, y_val.shape)
+print("Test dataset shape:", X_test.shape, y_test.shape)
 
 #%%
 
 # Assign training, validation and test samples as 80%, 10% and 10%, respectively
-train_size = int(0.8 * len(dataset))
-val_size = int(0.1 * len(dataset))
-#test_size = int(0.1 * len(dataset))
+# train_size = int(0.8 * len(dataframe))
+# val_size = int(0.1 * len(dataframe))
+# test_size = len(dataframe) - train_size - val_size
 
-# Create a copy of the original dataset to avoid modifications
-df_train_val = dataset.copy() 
+# # Create a copy of the original dataset to avoid modifications
+# df_train_val = dataframe.copy() 
 
-# Extract validation set first
-df_val = df_train_val.iloc[:val_size]
+# # Extract validation set first
+# df_val = df_train_val.iloc[:val_size]
 
-# Extract training set using calculated size
-df_train = df_train_val.iloc[val_size:val_size+train_size]
+# # Extract training set using calculated size
+# df_train = df_train_val.iloc[val_size:val_size+train_size]
 
-# Testing set remains separate (order preserved from original dataset)
-df_test = df_train_val.iloc[val_size+train_size:]
+# # Testing set remains separate (order preserved from original dataset)
+# df_test = df_train_val.iloc[val_size+train_size:]
 
 #%%
 
