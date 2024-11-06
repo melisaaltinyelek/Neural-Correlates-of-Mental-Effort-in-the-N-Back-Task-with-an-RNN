372b249c55252c0e490b602efff2306447b9a6df
Melisa Altinyelek <melisaaltinyelek@hotmail.com>
2024-11-06 15:09:02 +0100

    relabelled the data, added the method for multiclassification task, updated model architecture, evaluated performance for the lure trials

--- LSTM_model.py
+++ LSTM_model.py
@@ -13,14 +13,16 @@ import glob
 # %%
 
 class DataPreprocessor:
-    def __init__(self, data_path):
+    def __init__(self, data_path_bin_cl, data_path_multiclass_pred):
 
-        self.data_path = data_path
+        self.data_path_bin_cl = data_path_bin_cl
+        self.data_path_multiclass_pred = data_path_multiclass_pred
         self.df = None
+        self.df_with_lures = None
 
-    def preprocess_data(self):
+    def preprocess_data_for_bin_cl(self):
 
-        df = pd.read_csv(self.data_path)
+        df = pd.read_csv(self.data_path_bin_cl)
 
         df.replace(to_replace = "lure", value = "nontarget", inplace = True)
 
@@ -48,7 +50,30 @@ class DataPreprocessor:
         # print(self.df)
 
         return self.df
-  
+    
+    def preprocess_data_for_multic_pred(self):
+
+        df_with_lures = pd.read_csv(self.data_path_multiclass_pred)
+
+        response_multiclass_mapping = {
+            "nontarget": 0,
+            "target": 1,
+            "lure": 2
+        }
+
+        df_with_lures["response"] = df_with_lures["response"].map(response_multiclass_mapping)
+
+        test_letter_encoder = OneHotEncoder(sparse_output = False)
+        test_encoded_letters = test_letter_encoder.fit_transform(df_with_lures[["letter"]])
+
+        df_with_lures["letter"] = test_encoded_letters.tolist()
+        
+        self.df_with_lures = df_with_lures
+
+        # df_with_lures.to_csv("nback_data_with_lures.csv", index = False)
+
+        return self.df_with_lures
+        
     def create_sequences(self, df, n_steps = 4):
 
         X, y = [], []
@@ -81,7 +106,7 @@ class DataPreprocessor:
 
         return X, y
 
-    def split_data(self, data, train_ratio = 0.8):
+    def split_data_for_bin_pred(self, data, train_ratio = 0.8):
 
         X, y = self.create_sequences(data)
         
@@ -105,9 +130,24 @@ class DataPreprocessor:
         y_test = y[train_size + val_size:]
 
         return X_train, y_train, X_val, y_val, X_test, y_test
+    
+    def split_data_for_multic_pred(self, data):
+
+        X_test_with_lures, y_test_with_lures = self.create_sequences(data)
+
+        print(f"This is X_test with lures: {X_test_with_lures}")
+        print("-" * 50)
+        print(f"This is y_test with lures: {y_test_with_lures}")
+        print("-" * 50)
+        print(f"This is the length of X_test_with_lures: {len(X_test_with_lures)}")
+        print("-" * 50)
+        print(f"This is the length of y_test_with_lures: {len(y_test_with_lures)}")
+        print("-" * 50)
+
+        return X_test_with_lures, y_test_with_lures
 
 class LSTMTrainer:
-    def __init__(self, X_train, y_train, X_val, y_val, X_test, y_test, n_batch, learning_rate):
+    def __init__(self, X_train, y_train, X_val, y_val, X_test, y_test, X_test_with_lures, y_test_with_lures, n_batch, learning_rate):
 
         self.X_train = X_train
         self.y_train = y_train
@@ -115,6 +155,8 @@ class LSTMTrainer:
         self.y_val = y_val
         self.X_test = X_test
         self.y_test = y_test
+        self.X_test_with_lures = X_test_with_lures
+        self.y_test_with_lures = y_test_with_lures
         self.n_batch = n_batch
         self.learning_rate = learning_rate
         self.model = None
@@ -164,7 +206,7 @@ class LSTMTrainer:
 
         plt.xlabel("Epoch")
         plt.ylabel("Accuracy")
-        plt.legend(["Train", "Test"], loc = "upper right")
+        plt.legend(["Train", "Test"], loc = "lower right")
         plt.show()
 
         # Plot history for losses
@@ -177,7 +219,7 @@ class LSTMTrainer:
         plt.legend(["Train", "Test"], loc = "upper right")
         plt.show()
     
-    def eval_model(self):
+    def eval_model_without_lures(self):
 
         eval_results = self.model.evaluate(self.X_test, self.y_test, batch_size = 128)
         print(f"Overall Test Loss, Test Accuracy: {eval_results}")
@@ -186,8 +228,6 @@ class LSTMTrainer:
 
         predicted_responses = (predictions >= 0.5).astype(int).flatten()
 
-        # Print the first 10 predictions along with their true values
-        print("\nPredictions for 10 trials:")
         for i in range(10):
             letters_sequence = X_test[i]
 
@@ -203,23 +243,50 @@ class LSTMTrainer:
         
         return eval_results
 
+    def eval_model_with_lures(self):
+
+        predictions_with_lures = self.model.predict(self.X_test_with_lures)
+        predicted_labels = (predictions_with_lures >= 0.5).astype(int).flatten()
+
+        # print("All true labels in y_test_with_lures:", self.y_test_with_lures)
+
+        lure_count = 0 
+        for i, (pred, true_label) in enumerate(zip(predicted_labels, self.y_test_with_lures)):
+            if true_label == 2:  
+                lure_count += 1
+                letters_sequence = self.X_test_with_lures[i]
+                predicted_response = 'target' if pred == 1 else 'nontarget'
+
+                print(f"Lure Trial {i + 1}:")
+                print(f"Letters (sequence): {letters_sequence}")
+                print(f"True Label: Lure")
+                print(f"Predicted Response: {predicted_response}")
+                print("-" * 50)
+        
+        print(f"Total lure trials found: {lure_count}")
+
 #%%
 
 if __name__ == "__main__":
 
-    data_processor = DataPreprocessor("raw_data_with_lure.csv")  
-    preprocessed_data = data_processor.preprocess_data()
+    data_processor = DataPreprocessor(data_path_bin_cl = "raw_data_with_lure.csv", data_path_multiclass_pred = "raw_data_with_lure_test.csv")  
+    preprocessed_data_bin_cl = data_processor.preprocess_data_for_bin_cl()
+    preprocessed_data_multic_pred = data_processor.preprocess_data_for_multic_pred()
 
-    X_train, y_train, X_val, y_val, X_test, y_test = data_processor.split_data(preprocessed_data)
+    X_train, y_train, X_val, y_val, X_test, y_test = data_processor.split_data_for_bin_pred(preprocessed_data_bin_cl)
+    X_test_with_lures, y_test_with_lures = data_processor.split_data_for_multic_pred(preprocessed_data_multic_pred)
 
     print("Training dataset shape:", X_train.shape, y_train.shape)
     print("Validation dataset shape:", X_val.shape, y_val.shape)
     print("Test dataset shape:", X_test.shape, y_test.shape)
+    print("X test with lures shape", X_test_with_lures.shape)
+    print("y test with lures shape", y_test_with_lures.shape)
 
-    lstm_trainer = LSTMTrainer(X_train = X_train, y_train = y_train, X_val = X_val, y_val = y_val, X_test = X_test, y_test = y_test, n_batch = 64, learning_rate = 0.01)
+    lstm_trainer = LSTMTrainer(X_train = X_train, y_train = y_train, X_val = X_val, y_val = y_val, X_test = X_test, y_test = y_test, X_test_with_lures = X_test_with_lures, y_test_with_lures = y_test_with_lures, n_batch = 64, learning_rate = 0.01)
     lstm_trainer.initialize_model()
-    cce_history, model, training_history = lstm_trainer.train_model(epochs = 100)
+    bce_history, model, training_history = lstm_trainer.train_model(epochs = 100)
     display_acc_loss = lstm_trainer.visualize_results()
-    eval_results = lstm_trainer.eval_model()
+    eval_results_wo_lures = lstm_trainer.eval_model_without_lures()
+    eval_results_with_lures = lstm_trainer.eval_model_with_lures()
 
 # %%
